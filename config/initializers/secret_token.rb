b2bc859183034b8914e2adcc920ff239f5a0f9da
lajunta <home@zxy.com>
2013-10-04 23:39:20 +0800

    first commit in main

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Somachi::Application.config.secret_key_base = 'ac3971e38307471c8b2d488a820ec1c5fc42bbe488dc99d903b27e283c3467c0f025193836a6aebe1103eeb7ef7caa7e3d8b1f8af6fa8fe2af558e6f63579947'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Somachi::Application.config.session_store :cookie_store, key: '_somachi_session'
